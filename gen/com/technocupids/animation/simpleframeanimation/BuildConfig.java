459af8dd230753d05e879dcfa9c2a57ec8811c1b
Technocupids <technocupids@gmail.com>
2014-06-26 12:53:29 +0530

    Updated

---
+++ gen/com/technocupids/animation/simpleframeanimation/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.technocupids.animation.simpleframeanimation;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
